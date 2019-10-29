6583dbebf8e7b843ddad86be5466d4fbe1d2113c
madhusudan <madhusudhan638@gmail.com>
2019-10-29 22:26:40 +0530

    use socket.io on the server and as well as at the client side, set up express server to use socket.io library, as well as setup client side to communicate with the server

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 const express = require("express");
+const http = require("http");
 const path = require("path");
+const socketio = require("socket.io");
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
 const port = process.env.PORT || 3000;
 
 const publicDirPath = path.join(__dirname, "../public"); // __dirname gives path to index.js file
 
 app.use(express.static(publicDirPath));
 
-app.listen(port, () => {
+io.on("connection", () => {
+  console.log("new websocket connection");
+});
+
+server.listen(port, () => {
   console.log("Server is up on port " + port);
 });
